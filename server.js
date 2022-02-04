f8806441741735c7eb24d5c3ba3b5b188ca9cedf
thhuynh7 <thhuynh7@myseneca.ca>
2022-02-04 03:05:27 -0500

    1

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// const DB = 'mongodb+srv://dbUser:xxxMrceo@cluster0.2oil2.mongodb.net/sample_restaurants?retryWrites=true&w=majority';
-
 const DB = process.env.MONGO_URI;
 
 mongoose
